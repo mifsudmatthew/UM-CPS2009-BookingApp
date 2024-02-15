3b63fe26fcd0c2ab80c0250b31aeb1b7e20f7539
Keith Farrugia <keith.farrugia.22@um.edu.mt>
2024-02-15 19:12:27 +0100

    Server: Added Mongo and Mongoose connection and setup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,40 @@
 const path = require('path');
-const express = require('express');
+const express = require('express'); // npm i express
+const mongoose = require('mongoose'); // npm i mongoose
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// ========================= MONGOOSE =================================
+//Port Changed or updated depending on the generated port
+const mongoport = "mongodb://127.0.0.1:27017/database"; 
+
+const schema = require("./Mongoose/Database").default; // Loading the schema/Modal not yet used
+
+mongoose.connect(mongoport)
+  .then(() => {
+    console.log("Mongoose Successfully Connected");
+  })
+  .catch(err => {
+    // IF you want to see the full error remove the if statement 
+    // This is here to simplyfy the error message since it can be long
+    // this should only catch and cover up the error when the server doesn't connect 
+    // All other errors should be outputed as normal
+    if (err.name === 'MongooseServerSelectionError') {
+        console.log("Mongoose Connection Error: Failed to connect to database");
+        process.exit(1);
+    } else {
+        console.log("Mongoose Connection Error:", err);
+        process.exit(1); 
+    }
+  });
+// ========================= MONGOOSE =================================
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
+
+//========================================= Request Handling
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
@@ -18,4 +44,10 @@ app.get("/api", (req, res) => {
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+// ================================ Listening
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
 });
