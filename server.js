3cc038d3bab3b259a912feeb6b7ef1a31fa507bb
KeithFarrugia <keith.farrugia.22@um.edu.mt>
2024-02-19 12:33:58 +0100

    mongoose: Re-organization and basic saving to database

--- server.js
+++ server.js
@@ -1,45 +1,12 @@
 const path = require('path');
 const express = require('express'); // npm i express
-const mongoose = require('mongoose'); // npm i mongoose
+const env = require('dotenv').config()
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // ========================= MONGOOSE =================================
-//Port Changed or updated depending on the generated port
-const mongoport = "mongodb://127.0.0.1:27017/database"; 
 
-const schema = require("./Mongoose/Database").default; // Loading the schema/Modal not yet used
-
-mongoose.connect(mongoport)
-  .then(() => {
-    console.log("Mongoose Successfully Connected");
-  })
-  .catch(err => {
-    // IF you want to see the full error remove the if statement 
-    // This is here to simplyfy the error message since it can be long
-    // this should only catch and cover up the error when the server doesn't connect 
-    // All other errors should be outputed as normal
-    if (err.name === 'MongooseServerSelectionError') {
-        console.log("Mongoose Connection Error: Failed to connect to database");
-    } else {
-        console.log("Mongoose Connection Error:", err);
-    }
-  });
-// Connection event handlers
-mongoose.connection.on('disconnected', () => {
-  console.log("Mongoose Disconnected: Lost connection to database");
-});
-
-mongoose.connection.on('close', () => {
-  console.log("Mongoose Connection Closed: Database connection closed");
-});
-
-// Error event handler
-mongoose.connection.on('error', err => {
-  console.log("Mongoose Connection Error:", err);
-  process.exit(1);
-});
 // ========================= MONGOOSE =================================
 
 // Have Node serve the files for our built React app
