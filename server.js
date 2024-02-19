63b041a7fa9eeaabfef40096cab1d307fa25e83a
Luca Vella <luca.vella.22@um.edu.mt>
2024-02-19 12:22:57 +0100

    [UPDATE] Added POST support for form

--- server.js
+++ server.js
@@ -5,40 +5,43 @@ const mongoose = require('mongoose'); // npm i mongoose
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+let database_data = [];
+app.use(express.json());
+
 // ========================= MONGOOSE =================================
 //Port Changed or updated depending on the generated port
-const mongoport = "mongodb://127.0.0.1:27017/database"; 
+const mongoport = "mongodb://127.0.0.1:27017/database";
 
-const schema = require("./Mongoose/Database").default; // Loading the schema/Modal not yet used
+const schema = require("./mongoose/database").default; // Loading the schema/Modal not yet used
 
 mongoose.connect(mongoport)
-  .then(() => {
-    console.log("Mongoose Successfully Connected");
-  })
-  .catch(err => {
-    // IF you want to see the full error remove the if statement 
-    // This is here to simplyfy the error message since it can be long
-    // this should only catch and cover up the error when the server doesn't connect 
-    // All other errors should be outputed as normal
-    if (err.name === 'MongooseServerSelectionError') {
-        console.log("Mongoose Connection Error: Failed to connect to database");
-    } else {
-        console.log("Mongoose Connection Error:", err);
-    }
-  });
+    .then(() => {
+        console.log("Mongoose Successfully Connected");
+    })
+    .catch(err => {
+        // IF you want to see the full error remove the if statement 
+        // This is here to simplyfy the error message since it can be long
+        // this should only catch and cover up the error when the server doesn't connect 
+        // All other errors should be outputed as normal
+        if (err.name === 'MongooseServerSelectionError') {
+            console.log("Mongoose Connection Error: Failed to connect to database");
+        } else {
+            console.log("Mongoose Connection Error:", err);
+        }
+    });
 // Connection event handlers
 mongoose.connection.on('disconnected', () => {
-  console.log("Mongoose Disconnected: Lost connection to database");
+    console.log("Mongoose Disconnected: Lost connection to database");
 });
 
 mongoose.connection.on('close', () => {
-  console.log("Mongoose Connection Closed: Database connection closed");
+    console.log("Mongoose Connection Closed: Database connection closed");
 });
 
 // Error event handler
 mongoose.connection.on('error', err => {
-  console.log("Mongoose Connection Error:", err);
-  process.exit(1);
+    console.log("Mongoose Connection Error:", err);
+    process.exit(1);
 });
 // ========================= MONGOOSE =================================
 
@@ -49,7 +52,9 @@ app.use(express.static(path.resolve(__dirname, 'client/build')));
 //========================================= Request Handling
 
 // Handle GET requests to /api route
-app.get("/api", (req, res) => {
+app.post("/api", (req, res) => {
+    database_data.push(req.body);
+    console.log(database_data);
     res.json({ message: "Hello from server!" });
 });
 
