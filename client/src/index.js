aad23d1f9f9e62702a927caf2d3e3f245ed7400a
Luca Vella <luca.vella.22@um.edu.mt>
2024-02-19 16:08:05 +0100

    [Update] Changed Icons from defaults

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
 import './index.css';
 import App from './App';
 import BookingForm from './BookingForm'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
