f4e4a74794371258082a7702b64ed54611aa6811
Matthew Mifsud <matthewmifsud2004@gmail.com>
2024-02-19 01:29:14 +0100

    updating directory changes

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ mongoose.connect(mongoport)
 // ========================= MONGOOSE =================================
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, 'client/build')));
 
 
 //========================================= Request Handling
@@ -43,7 +43,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
 // ================================ Listening
