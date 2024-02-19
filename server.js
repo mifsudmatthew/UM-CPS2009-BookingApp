bda7584ae99d946226f04653a3ffba34f8597d91
Matthew Mifsud <matthewmifsud2004@gmail.com>
2024-02-19 14:43:57 +0100

    server.js [feature]: added log when /api receives request

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(express.static(path.resolve(__dirname, 'client/build')));
 // Handle GET requests to /api route
 app.post("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
+    console.log("Request has been received!")
 });
 
 // All other GET requests not handled before will return our React app
