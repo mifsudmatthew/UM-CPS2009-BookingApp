c0b185284c5addd76c9429c157c21297156bd4ee
lensil <147991201+lensil@users.noreply.github.com>
2024-02-19 14:19:33 +0100

    PWA update

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,19 @@ root.render(
   </React.StrictMode>
 );
 
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('Service Worker registration failed: ', registrationError);
+      });
+  });
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
