1c0b5b8a2f4fafcc08fe505ea14c16fbc2ce34b7
Luca Vella <luca.vella.22@um.edu.mt>
2024-02-20 12:09:20 +0100

    [Change] Spereate front page into files

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import './App.css'
 
+import BookingForm from './BookingForm'
+import Header from './Header'
+
 function App() {
     return (
-        <div className='background'>
-            <h1>Tennis Booking App</h1>
-            <div>
-                <a href="" target="_blank">
-                    <img src="https://static.vecteezy.com/system/resources/previews/027/504/381/original/a-tennis-ball-on-a-transparent-background-free-png.png" className="logo" alt="Tennis Booking App logo" />
-                </a>
-            </div>
-        </div>
+        <>
+            <Header />
+            <BookingForm />
+        </>
     )
 }
 
