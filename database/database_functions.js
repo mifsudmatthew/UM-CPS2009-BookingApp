3182f18c860851d3fb34016a8d3f68074522fc45
KeithFarrugia <keith.farrugia.22@um.edu.mt>
2024-02-22 17:23:22 +0100

    database_functions [Fix] : Fixed issues with database_function imports

--- database/database_functions.js
+++ database/database_functions.js
@@ -1,11 +1,15 @@
-const schema = require("./schemas"); // Importing the schemas created
-
+const booking_schema = require("./schemas/booking_schema"); // Importing the schemas created
+const user_schema = require("./schemas/user_schema");
+const courts_schema = require("./schemas/courts_schema");
 //====================== Basic Functions ====================
 
 //---------------------Basic Save Test------------------------
 async function saveTestCase(){ 
-    let item = {"username":"Tester", "password":"testing"}
-    const dbpush = new schema(item);
+    let item = {    "email":"tester@gmail.com",
+                    "password":"testing",
+                    "name": "tester101",
+                    "balance": 123}
+    const dbpush = new user_schema(item);
     await dbpush.save();
 }
 
