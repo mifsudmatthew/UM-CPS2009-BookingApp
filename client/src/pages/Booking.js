099f945452c769ae55f75c13738178d0f94b085d
Luca Vella <luca.vella.22@um.edu.mt>
2024-02-25 22:11:24 +0100

    [Change] Added routing and consistent organization

---
+++ client/src/pages/Booking.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import BookingForm from "../components/BookingForm";
+
+function Booking() {
+  return (
+    <>
+      <Header />
+      <BookingForm />
+    </>
+  );
+}
+
+export default Booking;
