8cdb8fab33d8bc5bdae64c0b2b126e11b8d6d8d5
Luca Vella <luca.vella.22@um.edu.mt>
2024-02-26 22:04:05 +0100

    [Feature] Updated Icon to be keith's tennis ball and added PWA back

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,6 +11,39 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      manifest: {
+        name: "Servespot - Tennis Court Booking",
+        short_name: "ServeSpot",
+        description: "Book a tennis court in advance",
+        theme_color: "#ffffff",
+        background_color: "#FFFFFF",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icon-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
     }),
   ],
   build: { manifest: true, outDir: "build/" },
