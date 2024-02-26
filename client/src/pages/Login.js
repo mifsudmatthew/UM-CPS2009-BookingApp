0ba9dca1ccb937cb4000c64e587f7c9d6a9141b3
lensil <147991201+lensil@users.noreply.github.com>
2024-02-26 13:34:50 +0100

    [Feature] Added login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,36 @@
-export default function Login() {
+import React from 'react';
+import "../styles/Login.css";
+
+class Login extends React.Component {
+  render() {
   return (
-    <>
-      <h1>Reset</h1>
-    </>
-  );
+    <> 
+    <div className="signin-container">
+    <h2>Sign in</h2>
+    <form>
+      <div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <br/>
+            <input type="email" placeholder="name@example.com" required/>
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <br/>
+            <input type="password" placeholder="password" required/>
+            <br/>
+            <a href="" className="forgot-password">Forgot password?</a>
+          </div>
+      </div>
+      <button type="submit" className="signin-button">Sign in</button>
+    </form>
+    <div className="signup">
+      Not a member? <a href="#" className="signup-link">Sign up</a>
+    </div>
+  </div>
+  </>
+    );
+  }
 }
+
+export default Login;
