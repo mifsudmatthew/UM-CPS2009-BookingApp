73e1b2fefccc2061c79c3679eb7f6e7c24d812ce
Luca Vella <luca.vella.22@um.edu.mt>
2024-02-26 22:40:52 +0100

    [Fix] Final touches on Service worker

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,16 @@
+/* index.jsx
+ * Entry point into react application
+ * (Acts as the root of all javascript) */
+
+/* React commonents to render */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+/* Main CSS and React entry point (App) */
 import App from "./App.jsx";
 import "./styles/index.css";
 
+// Modifies div with root id in index.html
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: "prompt",
       devOptions: {
         enabled: true,
       },
@@ -23,11 +22,6 @@ export default defineConfig({
           {
             src: "/icon-192x192.png",
             sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
             type: "image/png",
           },
           {
