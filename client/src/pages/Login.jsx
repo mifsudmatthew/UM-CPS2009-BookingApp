abfe3ad33f523c7609ed01ece0eeb0bd82a1aa31
lensil <147991201+lensil@users.noreply.github.com>
2024-02-27 10:18:41 +0100

    [Feature] Send login details to server

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,49 @@
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Save the email and password from the form
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  // Send the login details to the server
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      email,
+      password,
+    };
+    console.log("Data:", data);
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        // Handle response 
+        console.log("Success:", data);
+      } else {
+        // Handle errors 
+        console.error("Error:", response);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+
+  // Form to input login detils
   return (
     <div className={"mainContainer"}>
       <div className={"titleContainer"}>
