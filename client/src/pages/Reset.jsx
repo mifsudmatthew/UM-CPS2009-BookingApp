c5d28b231bf2dacfd6a676281ba792cb1c8961e8
lensil <147991201+lensil@users.noreply.github.com>
2024-02-27 13:35:38 +0100

    [Feature] Reset Form

--- client/src/pages/Reset.jsx
+++ client/src/pages/Reset.jsx
@@ -1,8 +1,24 @@
+import React, { useState, useEffect } from "react";
+
 export default function Reset() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordMatch, setPasswordMatch] = useState(true);
+
+  useEffect(() => {
+    setPasswordMatch(password === confirmPassword);
+  }
+  , [password, confirmPassword]);
 
   const handleSubmit = async (event) => {
+    const data = {
+      email,
+      password
+
+    }
     event.preventDefault();
-    console.log("Data:");
+    console.log("Data:", data);
     try {
       const response = await fetch("/api/reset", {
         method: "GET"
@@ -24,7 +40,44 @@ export default function Reset() {
   return (
     <>
       <h1>Reset</h1>
+      <div className={"mainContainer"}>
+      <div className={"inputContainer"}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        onChange={(event) => setConfirmPassword(event.target.value)}
+      />
+      </div>
+      <br />
+      {!passwordMatch && (
+        <div style={{ color: "rgba(186, 26, 26, 1)" }}>
+          Passwords do not match.
+        </div>
+      )}
+      <br />
+      <div className={"inputContainer"}>
       <button onClick={handleSubmit}>RESET PASSWORD</button>
+      </div>
+      </div>
     </>
 
   );
