07a56008f7e4fa24fba4f71ee33ac937e867c350
Luca Vella <luca.vella.22@um.edu.mt>
2024-02-27 13:12:33 +0100

    [Change] Moved API calls to seperate file

---
+++ api.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const apiRouter = express.Router();
+
+const mongoose = require("./database/mongoose"); // Running the database
+const db = require("./database/test_functions");
+const serverFunctions = require("./server_functions");
+const queries = require("./database/schema_functions/user_functions");
+
+apiRouter.use((req, res, next) => {
+  console.log(`API on ${req.url}`);
+  next();
+});
+
+apiRouter.get("/reset", (req, res) => {
+  console.log("Connected to reset page");
+  serverFunctions.sendPinByMail(res);
+});
+
+apiRouter.post("/booking", (req, res, next) => {
+  console.log("Booking request has been received!");
+  db.saveTestCase();
+  res.json({ message: "Booking added" });
+});
+
+apiRouter.post("/login", (req, res, next) => {
+  console.log("Login attempt");
+  console.log(req.body);
+  res.json({ token: "1234" });
+});
+
+apiRouter.post("/register", async (req, res, next) => {
+  console.log(
+    await queries.registerUser({
+      email_new: req.body.email,
+      password_new: req.body.password,
+      name_new: req.body.name,
+    })
+  );
+  res.status(69).end();
+});
+
+module.exports = apiRouter;
