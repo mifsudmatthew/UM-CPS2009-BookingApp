cef4f4e91e8d53debd65a93a495fff2530e225cc
lensil <147991201+lensil@users.noreply.github.com>
2024-03-04 12:45:26 +0100

    [Feature] Added about us page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
+import AboutUs from "../components/AboutUs";
 
 function Home() {
   return (
     <>
       <Banner />
+      <AboutUs />
     </>
   );
 }
