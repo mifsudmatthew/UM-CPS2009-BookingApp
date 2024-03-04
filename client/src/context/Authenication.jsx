9f9d712d2478864310b96b85cf4482fec4766533
Luca Vella <luca.vella.22@um.edu.mt>
2024-03-04 01:36:00 +0100

    [Feature] Tokens on client

--- client/src/context/Authenication.jsx
+++ client/src/context/Authenication.jsx
@@ -1,3 +1,3 @@
 import { createContext } from "react";
 
-export const Auth = createContext("");
+export const AuthContext = createContext("");
---
+++ client/src/hooks/useToken.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export function useToken() {
+  const getToken = () => {
+    console.log("Retrieving token from localStorage");
+    const token = localStorage.getItem("token");
+
+    if (token == null) {
+      return "";
+    } else {
+      return token;
+    }
+  };
+
+  const [token, setToken] = useState(getToken());
+
+  const saveToken = (userToken) => {
+    console.log("Setting token in localStorage");
+
+    localStorage.setItem("token", JSON.stringify(userToken));
+    setToken(userToken.token);
+  };
+
+  return {
+    setToken: saveToken,
+    token,
+  };
+}
