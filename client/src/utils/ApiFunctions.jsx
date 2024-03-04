31da97ebeaad8584fbdbf68a68e85f9418474e6a
Luca Vella <luca.vella.22@um.edu.mt>
2024-03-05 00:57:59 +0100

    [Fix] Possible break in token

--- client/src/utils/ApiFunctions.jsx
+++ client/src/utils/ApiFunctions.jsx
@@ -2,7 +2,9 @@ export function Post(url, data) {
   return fetch(url, {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      Authorization: `Bearer ${JSON.parse(
+        localStorage.getItem("accessToken")
+      )}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
