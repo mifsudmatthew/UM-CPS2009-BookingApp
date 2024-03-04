10621b5705ef5c1c882576f56f9948ed4ea4d6c1
lensil <147991201+lensil@users.noreply.github.com>
2024-03-04 13:50:34 +0100

    [Feature] Added Top Up Page

--- client/src/pages/Topup.jsx
+++ client/src/pages/Topup.jsx
@@ -1,8 +1,46 @@
+import { useState } from "react";
+import { Post } from "../utils/ApiFunctions";
+import { useToken } from "../hooks/useToken";
+
 function Topup() {
+  const [amount, setAmount] = useState(""); 
+  const { token } = useToken(); 
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const numericAmount = parseFloat(amount);
+
+    try {
+      console.log("Amount: ", numericAmount);
+      const data = await Post("/api/topup", { amount: numericAmount }, token);
+      console.log(data);
+    } catch (error) {
+      console.error(`Error in top-up: ${error}`);
+    }
+  };
+
   return (
-    <>
-      <h1>Top up</h1>
-    </>
+    <div className="mainContainer">
+      <h1 className="titleContainer">Top Up</h1>
+      <form onSubmit={handleSubmit} className="inputContainer">
+          <label>Amount</label>
+          <input
+            placeholder="€1000"
+            className="inputBox"
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            required
+          />
+        <br />
+        <input
+          className={"inputButton"}
+          type="button"
+          value={"Top Up"}
+          onClick={handleSubmit}
+        />
+      </form>
+    </div>
   );
 }
 
