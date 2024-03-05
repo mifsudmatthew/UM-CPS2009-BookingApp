7e20510d7487dd02a09633a2ba8ac251ecd15377
lensil <147991201+lensil@users.noreply.github.com>
2024-03-05 14:06:21 +0100

    [Feature] Updated the balance page

--- client/src/components/Balance.jsx
+++ client/src/components/Balance.jsx
@@ -1,7 +1,25 @@
+import { Link} from "react-router-dom";
+
 const Balance = () => {
+    const currentBalance = 1250.75;
     return (
     <main className="profile">
         <div className="header-title">Balance</div> 
+        <section>
+          <h4>
+            Your current balance is: {currentBalance}
+          </h4>
+        </section>
+        <section>
+        <div className="signup">
+        <h4>
+         Do you wish to top up your balance?{" "}
+        <Link to="/profile/topup" className="signup-link">
+          Click here
+        </Link>
+        </h4>
+      </div>
+        </section>
       </main>
     );
 }
