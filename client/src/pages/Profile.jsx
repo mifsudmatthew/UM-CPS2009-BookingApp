b3085ee1dc45c59c03719f2e95fdc56111e730ed
Luca Vella <luca.vella.22@um.edu.mt>
2024-03-18 11:14:18 +0100

    [Feature] Log out button

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import '../styles/profile.css';
-import ProfileSidebar from '../components/ProfileSidebar';
-import AccountDetails from '../components/AccountDetails';
-import Bookings from '../components/Bookings';
-import Balance from '../components/Balance';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import "../styles/profile.css";
+import ProfileSidebar from "../components/ProfileSidebar";
+import AccountDetails from "../components/AccountDetails";
+import Bookings from "../components/Bookings";
+import Balance from "../components/Balance";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import { useToken } from "../hooks/useToken";
-
+import { useAuth } from "../context/Authenication";
 
 const Profile = () => {
-  const { accessToken } = useToken()
+  const auth = useAuth();
 
-  if (accessToken == "") {
+  if (auth == "") {
+    setTimeout(1000);
     return <Navigate to="/login" replace />;
   }
 
