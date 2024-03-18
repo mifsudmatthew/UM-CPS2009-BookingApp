6e671b8ee01895cb294a4a89542026d1709334d0
Luca Vella <luca.vella.22@um.edu.mt>
2024-03-18 14:00:42 +0100

    [Fix] I messed up

--- client/src/context/Auth.jsx
+++ client/src/context/Auth.jsx
@@ -1,18 +1,17 @@
 import { useState, createContext, useContext } from "react";
 
+const AuthContext = createContext("");
+
 export default function Auth() {
-  const AuthContext = createContext("");
-  const [accessToken, _setAccessToken] = useState(getAccessToken());
+  const [accessToken, _setAccessToken] = useState(getAccessToken);
 
   function getAccessToken() {
-    const _token = JSON.parse(localStorage.getItem("accessToken"));
-    return _token || "";
+    return localStorage.getItem("accessToken") || "";
   }
 
-  function setToken(accessToken) {
-    const _token = JSON.stringify(accessToken);
-    localStorage.setItem("accessToken", _token);
-    _setAccessToken(_token);
+  function setToken(token) {
+    localStorage.setItem("accessToken", JSON.stringify(token));
+    _setAccessToken(token);
   }
 
   const AuthProvider = ({ children }) => {
