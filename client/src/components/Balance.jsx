2072f3d9cc45e979742a113a71365eaba2dc3707
lensil <147991201+lensil@users.noreply.github.com>
2024-04-08 15:10:50 +0200

    AboutUs.jsx & Balance.jsx [Cln] : Added comments

--- client/src/components/Balance.jsx
+++ client/src/components/Balance.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 import { useUser } from "../context/User";
 
+/**
+ * Renders the Balance component.
+ *
+ * @returns {JSX.Element} The rendered Balance component.
+ */
 const Balance = () => {
   const { user, setUser } = useUser();
 
@@ -8,10 +13,10 @@ const Balance = () => {
     <main className="profile">
       <div className="header-title">Balance</div>
       <section>
-        {user.balance == undefined ? (
-          <h4>Balance not available</h4>
-        ) : (
-          <h4>Your current balance is: {user.balance}</h4>
+        {user.balance == undefined ? ( // Check if the user's balance is undefined
+          <h4>Balance not available</h4> // Display a message if the balance is undefined
+        ) : ( // If the balance is defined
+          <h4>Your current balance is: {user.balance}</h4> // Display the user's balance
         )}
       </section>
       <section>
