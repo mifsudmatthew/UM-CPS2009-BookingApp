75e4b65f59b4225db739b6a4f5d4a98be5d336dc
lensil <147991201+lensil@users.noreply.github.com>
2024-04-08 15:01:23 +0200

    Banner.jsx [Cln] : Added comments

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -5,22 +5,34 @@ import { useAuth } from "../context/Auth";
 
 import "../styles/banner.css";
 
+/**
+ * Renders a banner component with a title, description, and a login button.
+ * 
+ * @returns {JSX.Element} The Banner component.
+ */
 function Banner() {
   const navigate = useNavigate();
   const { user } = useUser();
   const { token } = useAuth();
   const [buttonText, setButtonText] = useState("");
 
+  // Set the button text based on the user's login status
   useEffect(() => {
-    if (token === "") {
-      setButtonText("Login");
-    } else if (!user.admin) {
-      setButtonText("Book a court");
-    } else {
-      setButtonText("Admin Panel");
+    if (token === "") {  // If the user is not logged in
+      setButtonText("Login"); // Set the button text to "Login"
+    } else if (!user.admin) { // If the user is logged in and not an admin
+      setButtonText("Book a court"); // Set the button text to "Book a court"
+    } else { // If the user is an admin
+      setButtonText("Admin Panel"); // Set the button text to "Admin Panel"
     }
-  }, [token, user.admin]);
+  }, [token, user.admin]); 
 
+  /**
+   * 
+   * Redirects the user to the login page if they are not logged in.
+   * If the user is logged in, redirects them to the booking page if they are not an admin.
+   * If the user is an admin, redirects them to the admin page.
+   */
   const toLogin = () => {
     if (token === "") {
       navigate("/login", { replace: true });
