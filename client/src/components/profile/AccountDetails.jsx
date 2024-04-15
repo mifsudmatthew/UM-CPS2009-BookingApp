381d5c25ac17113bff4a8aa5cc9d22e34ed1db25
Matthew Mifsud <matthewmifsud2004@gmail.com>
2024-04-15 16:24:07 +0200

    api.js & user_functions.js & AccountDetails.jsx [Feature]: Added all required functionality for updating user name and email.

--- client/src/components/profile/AccountDetails.jsx
+++ client/src/components/profile/AccountDetails.jsx
@@ -3,26 +3,65 @@ import { useUser } from "../../context/UserContext";
 import Form from "../form/Form";
 import InputBox from "../form/InputBox";
 import InputButton from "../form/InputButton";
+import { Post } from "../../utils/ApiFunctions";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 /**
  * Renders the AccountDetails component.
  *
  * @returns {JSX.Element} The rendered AccountDetails component.
  */
+
+// Regular expression for email validation
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 const AccountDetails = () => {
   const { user, setUser } = useUser(); // Retrieve user data once when the component mounts
+  const { updateToken } = useAuth();
+  const navigate = useNavigate();
 
   // Initialize state with values from localStorage, or fallback to empty strings
   const [name, setName] = useState(user.name || "");
   const [email, setEmail] = useState(user.email || "");
 
-  const handleSubmit = (event) => {
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   setUser({ ...user, name, email });
+  // };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    setUser({ ...user, name, email });
+
+    // Check if the email is valid
+    if (!emailRegex.test(email)) {
+      toast.error("Invalid email format detected.");
+      return;
+    }
+
+    try {
+      const response = await Post("/api/changedetails", { name, email });
+      const { accessToken, ...user } = response.data;
+
+      updateToken(accessToken);
+      setUser(user);
+
+      toast.success("Change successful!");
+      setTimeout(() => {
+        // navigate("/profile", { replace: true });
+        window.location.reload();
+      }, 2000);
+    } catch (error) {
+      console.error(`Error in: ${error}`);
+      toast.error("Change failed!");
+    }
   };
 
   return (
     <main className="profile">
+      <ToastContainer />
       <div className="header-title">Profile</div>
       <Form>
         <InputBox
