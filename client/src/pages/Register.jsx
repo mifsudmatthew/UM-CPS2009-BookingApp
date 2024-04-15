d5f692fc44ebb43a442cbd71f6d8ebd163b66e61
Luca Vella <luca.vella.22@um.edu.mt>
2024-04-15 11:30:21 +0200

    [Fix] Updated token correctly on topup

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -41,43 +41,44 @@ function Register() {
     }
 
     if (!emailRegex.test(email)) {
-      toast.error("Invalid email format detected."); // Displays an error toast if the email format is invalid
+      // Displays an error toast if the email format is invalid
+      toast.error("Invalid email format detected.");
       return;
     }
 
     if (!emailMatch) {
-      toast.error("Emails do not match."); // Displays an error toast if the email and confirm email do not match
+      // Displays an error toast if the email and confirm email do not match
+      toast.error("Emails do not match.");
       return;
     }
 
     if (!passwordMatch) {
-      toast.error("Passwords do not match."); // Displays an error toast if the password and confirm password do not match
+      // Displays an error toast if the password and confirm password do not match
+      toast.error("Passwords do not match.");
       return;
     }
 
     try {
-      const response = await Post("/api/register", {
-        email,
-        password,
-        name,
-      }); // Sends a POST request to the "/api/register" endpoint with the data object
-      toast.success("Sign up successful! Redirecting to login."); // Displays a success toast message
+      // Sends a POST request to "/api/register" with the data
+      await Post("/api/register", { email, password, name });
+      // Displays a success toast message
+      toast.success("Sign up successful! Redirecting to login.");
+      // Redirects to the login page after a delay
       setTimeout(() => {
-        navigate("/login", { replace: true }); // Redirects to the login page after a delay
+        navigate("/login", { replace: true });
       }, 2000);
-      console.log("Success:", response); // Logs the response object to the console
     } catch (err) {
-      toast.error("Account with the same E-mail already exists."); // Displays an error toast if an account with the same email already exists
-      console.error(err); // Logs the error to the console
+      // Displays an error toast if an account with the same email already exists
+      toast.error("Account with the same E-mail already exists.");
+      // Logs the error to the console
+      console.error(err);
     }
   };
 
   return (
     <div className={"mainContainer"}>
       <ToastContainer /> {/* Container for displaying toast messages */}
-      <div className={"titleContainer"}>
-        Create Account {/* Title for the register page */}
-      </div>
+      <h2 className="titleContainer">Create Account</h2>
       <br />
       <Form className="innerContainer">
         <InputBox
