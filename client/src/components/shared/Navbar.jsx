00ae246c1bd7c8e791993f9785441dab2c38cd6d
Matthew Mifsud <matthewmifsud2004@gmail.com>
2024-04-16 14:37:12 +0200

    Navbar.jsx [Fix]: Removed excess

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -53,19 +53,15 @@ const isAdmin = (user) => {
  */
 function Navbar() {
   const [open, setOpen] = useState(false);
-
   let menuRef = useRef();
 
   useEffect(() => {
     let handler = (e) => {
       if (!menuRef.current.contains(e.target)) {
         setOpen(false);
-        console.log(menuRef.current);
       }
     };
-
     document.addEventListener("mousedown", handler);
-
     return () => {
       document.removeEventListener("mousedown", handler);
     };
