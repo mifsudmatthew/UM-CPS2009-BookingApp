c99a3fc7d4adf190a5b160b8ecd2a0043aff306b
Keith Farrugia <keith.farrugia.22@um.edu.mt>
2024-04-16 14:09:47 +0200

    send help

--- server/post/bookings_post.js
+++ server/post/bookings_post.js
@@ -54,13 +54,69 @@ bookingRouter.post(
   }
 );
 
-bookingRouter.post("/getFutureBookings", async (req, res) => {
-  email = req.user.email;
-  user = await user_quieries.retrieveUser(email);
-  console.log(user);
-  var responseQ = await bookings_quieries.getFutureBookings_ID(user.data.id);
+bookingRouter.post(
+  "/getFutureBookings",
+  server_functions.authenticateToken,
+  async (req, res) => {
+    try {
+      const email = req.user.email;
+      const user = await user_quieries.retrieveUser(email);
+      const bookings = await bookings_quieries.getFutureBookings_ID(
+        user.data.id
+      );
+      if(bookings.result == true){
+        const formattedBookings = await Promise.all(
+          bookings.data.map(async (booking) => {
+            const court = await courts_quieries.retrieveCourt(booking.courtID);
+            return {
+              id: booking.id,
+              date: booking.date.toDateString(),
+              time: booking.time,
+              name: court.data.court_name,
+              address: court.data.address,
+              price: court.data.price,
+            };
+          })
+        );
+        res.json(formattedBookings);
+      }else{
+        res.json([]);
+      }
+    } catch (error) {
+      console.error("Error fetching future bookings: ", error);
+      res.status(500).json({ error: "Error fetching future bookings" });
+    }
+  }
+);
+bookingRouter.post(
+  "/cancelBooking",
+  server_functions.authenticateToken,
+  async (req, res) => {
+    try {
+      const booking_id = req.body.booking_id;
+      const result = await bookings_quieries.removeBooking(
+        booking_id
+      );
+      if(result.result == true){
+        await user_quieries.updateUserBalance(req.user.email, req.body.price);
+        const user = await user_queries.retrieveUser(email);
+        const userData = {
+          _id: user.data._id,
+          email: user.data.email,
+          name: user.data.name,
+          admin: user.data.admin,
+          balance: user.data.balance,
+        };
+        const accessToken = server_functions.generateAccessToken(userData);
 
-  res.json(responseQ.data);
-});
-
+        return res.json({ result: true, accessToken: accessToken });
+      }else{
+        res.json({result: false});
+      }
+    } catch (error) {
+      console.error("Error fetching future bookings: ", error);
+      res.status(500).json({ error: "Error fetching future bookings" });
+    }
+  }
+);
 module.exports = bookingRouter;
