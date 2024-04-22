ad081fadd26df5fcbfb27811bd0731d252730bf8
lensil <147991201+lensil@users.noreply.github.com>
2024-04-22 14:32:11 +0200

    Footer.jsx & footer.css [Chg]: updated footer

--- client/src/components/shared/Footer.jsx
+++ client/src/components/shared/Footer.jsx
@@ -4,11 +4,32 @@ import "../../styles/footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <img src={logo}></img>
+      <div>
+      <br />
+        <div className="footer-content">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="footer-bottom">
+          <h5>© 2024 ServeSpot</h5>
+        </div>
       </div>
-      <div className="footer-bottom">
-        <h5>© 2024 ServeSpot</h5>
+      <div>
+        <nav>
+            <h4>Links</h4>
+              <a href="/">Home</a>
+              <br />
+              <a href="/login">Login</a>
+              <br />
+              <a href="/register">Register</a>
+        </nav>
+      </div>
+      <div>
+        <h4>Contact us</h4>
+        <h8>Email: servespot@servspot.com</h8>
+        <br />
+        <h8>Telephone: 123456789</h8>
+        <br />
+        <h8>Address: Malta, Malta, MLT123</h8>
       </div>
     </footer>
   );
