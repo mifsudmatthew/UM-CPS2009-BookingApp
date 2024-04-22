1a24f26b9cf07b047d0951ba6c197327b1db6578
Keith Farrugia <keith.farrugia.22@um.edu.mt>
2024-04-22 17:46:48 +0200

    CourtBookingStats.jsx [Feat] : We now have statistics displaying in musical instruments

--- client/src/components/admin/CourtBookingStats.jsx
+++ client/src/components/admin/CourtBookingStats.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import Accordion from "react-bootstrap/Accordion";
 import { Post } from "../../utils/ApiFunctions"; // Import the Post function to make API requests
 import "../../styles/accordion.css";
 
@@ -27,18 +26,49 @@ function CourtBookingStats() {
         fetchCourtBookings();
     }, []); // Ensure useEffect runs only once on component mount
 
+    const toggleAccordion = (index) => {
+        setCourtStats((prevStats) =>
+            prevStats.map((court, i) =>
+                i === index ? { ...court, isOpen: !court.isOpen } : court
+            )
+        );
+    };
+
     return (
-        <Accordion>
+        <div className="accordion">
             {courtStats.map((court, index) => (
-                <Accordion.Item key={index} eventKey={index.toString()}>
-                    <Accordion.Header>{court.name}</Accordion.Header>
-                    <Accordion.Body>
-                        <p>Number of Bookings: {court.bookings}</p>
-                        <p>Total Money Made: {court.money}</p>
-                    </Accordion.Body>
-                </Accordion.Item>
+                <div key={index} className="accordion-item">
+                    <div
+                        className={`accordion-header ${
+                            court.isOpen ? "open" : "close"
+                        }`}
+                        onClick={() => toggleAccordion(index)}
+                    >
+                        {court.name}
+                    </div>
+                    <div
+                        className={`accordion-body ${
+                            court.isOpen ? "open" : "close"
+                        }`}
+                    >
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Bookings Count</th>
+                                    <th>Total Profit</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{court.bookings}</td>
+                                    <td>{court.money}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             ))}
-        </Accordion>
+        </div>
     );
 }
 
