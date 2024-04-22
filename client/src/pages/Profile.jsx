a8b6387e58c0741d4b1236e30ae29ee3d12297d9
Luca Vella <luca.vella.22@um.edu.mt>
2024-04-22 13:01:10 +0200

    profile fix

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,17 +13,17 @@ import Admin from "../components/shared/Admin.jsx";
  */
 const Profile = () => {
   return (
-   //<Authenticated>
-   <div>
-      <Admin>
-        <Navigate to="/" replace={true} />
-      </Admin>
-      <div className="profile-container">
-        <ProfileSidebar />
-        <Outlet />
+    <Authenticated>
+      <div>
+        <Admin>
+          <Navigate to="/" replace={true} />
+        </Admin>
+        <div className="profile-container">
+          <ProfileSidebar />
+          <Outlet />
+        </div>
       </div>
-      </div>
-    //</Authenticated>
+    </Authenticated>
   );
 };
 
