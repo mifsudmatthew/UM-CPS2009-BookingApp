0e89464ae8cc3d1fcc0bc6693b29f95e1363003c
Matthew Mifsud <matthewmifsud2004@gmail.com>
2024-04-22 18:02:20 +0200

    ConfigCourts.jsx [Feature]: Input validation WIP

--- client/src/components/admin/CourtBookingStats.jsx
+++ client/src/components/admin/CourtBookingStats.jsx
@@ -8,38 +8,38 @@ import "../../styles/accordion.css";
  * @returns {JSX.Element} The CourtBookingStats component.
  */
 function CourtBookingStats() {
-    const [courtStats, setCourtStats] = useState([]); // Array of court bookings
+  const [courtStats, setCourtStats] = useState([]); // Array of court bookings
 
-    useEffect(() => {
-        /**
-         * Fetches a list of courts and associated bookings from the server.
-         * @returns {Promise<void>} A Promise that resolves when the court bookings are fetched successfully.
-         */
-        const fetchCourtBookings = async () => {
-            try {
-                const response = await Post("/api/getBasicStatistics");
-                console.log(response);
-                setCourtStats(response.data); // Update state with the received court statistics
-            } catch (error) {
-                console.error(`Error fetching courts: ${error}`);
-            }
-        };
-        fetchCourtBookings();
-    }, []); // Ensure useEffect runs only once on component mount
+  useEffect(() => {
+    /**
+     * Fetches a list of courts and associated bookings from the server.
+     * @returns {Promise<void>} A Promise that resolves when the court bookings are fetched successfully.
+     */
+    const fetchCourtBookings = async () => {
+      try {
+        const response = await Post("/api/getBasicStatistics");
+        console.log(response);
+        setCourtStats(response.data); // Update state with the received court statistics
+      } catch (error) {
+        console.error(`Error fetching courts: ${error}`);
+      }
+    };
+    fetchCourtBookings();
+  }, []); // Ensure useEffect runs only once on component mount
 
-    return (
-        <Accordion>
-            {courtStats.map((court, index) => (
-                <Accordion.Item key={index} eventKey={index.toString()}>
-                    <Accordion.Header>{court.name}</Accordion.Header>
-                    <Accordion.Body>
-                        <p>Number of Bookings: {court.bookings}</p>
-                        <p>Total Money Made: {court.money}</p>
-                    </Accordion.Body>
-                </Accordion.Item>
-            ))}
-        </Accordion>
-    );
+  return (
+    <Accordion>
+      {courtStats.map((court, index) => (
+        <Accordion.Item key={index} eventKey={index.toString()}>
+          <Accordion.Header>{court.name}</Accordion.Header>
+          <Accordion.Body>
+            <p>Number of Bookings: {court.bookings}</p>
+            <p>Total Money Made: {court.money}</p>
+          </Accordion.Body>
+        </Accordion.Item>
+      ))}
+    </Accordion>
+  );
 }
 
 export default CourtBookingStats;
