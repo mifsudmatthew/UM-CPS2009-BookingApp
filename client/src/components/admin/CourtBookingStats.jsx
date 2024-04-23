504886e6d8fedd8d3d2d978a77012fdc2ddc3e34
Matthew Mifsud <matthewmifsud2004@gmail.com>
2024-04-23 10:27:37 +0200

    Fixing merge conflict.

--- client/src/components/admin/CourtBookingStats.jsx
+++ client/src/components/admin/CourtBookingStats.jsx
@@ -26,66 +26,44 @@ function CourtBookingStats() {
     fetchCourtBookings();
   }, []); // Ensure useEffect runs only once on component mount
 
-<<<<<<< HEAD
-  return (
-    <Accordion>
-      {courtStats.map((court, index) => (
-        <Accordion.Item key={index} eventKey={index.toString()}>
-          <Accordion.Header>{court.name}</Accordion.Header>
-          <Accordion.Body>
-            <p>Number of Bookings: {court.bookings}</p>
-            <p>Total Money Made: {court.money}</p>
-          </Accordion.Body>
-        </Accordion.Item>
-      ))}
-    </Accordion>
-  );
-=======
-    const toggleAccordion = (index) => {
-        setCourtStats((prevStats) =>
-            prevStats.map((court, i) =>
-                i === index ? { ...court, isOpen: !court.isOpen } : court
-            )
-        );
-    };
-
-    return (
-        <div className="accordion">
-            {courtStats.map((court, index) => (
-                <div key={index} className="accordion-item">
-                    <div
-                        className={`accordion-header ${
-                            court.isOpen ? "open" : "close"
-                        }`}
-                        onClick={() => toggleAccordion(index)}
-                    >
-                        {court.name}
-                    </div>
-                    <div
-                        className={`accordion-body ${
-                            court.isOpen ? "open" : "close"
-                        }`}
-                    >
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Bookings Count</th>
-                                    <th>Total Profit</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{court.bookings}</td>
-                                    <td>{court.money}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            ))}
-        </div>
+  const toggleAccordion = (index) => {
+    setCourtStats((prevStats) =>
+      prevStats.map((court, i) =>
+        i === index ? { ...court, isOpen: !court.isOpen } : court
+      )
     );
->>>>>>> c26325b39ad8747b3ce1aa60522c49fdd16fc091
+  };
+
+  return (
+    <div className="accordion">
+      {courtStats.map((court, index) => (
+        <div key={index} className="accordion-item">
+          <div
+            className={`accordion-header ${court.isOpen ? "open" : "close"}`}
+            onClick={() => toggleAccordion(index)}
+          >
+            {court.name}
+          </div>
+          <div className={`accordion-body ${court.isOpen ? "open" : "close"}`}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Bookings Count</th>
+                  <th>Total Profit</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{court.bookings}</td>
+                  <td>{court.money}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default CourtBookingStats;
