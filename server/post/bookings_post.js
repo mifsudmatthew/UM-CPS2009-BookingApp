e7853e841acbb0dc06d0047da8d168e8dd15cbec
Matthew Mifsud <matthewmifsud2004@gmail.com>
2024-04-30 17:48:00 +0200

    bookings_post [Fixed]: Cancellation email data corrected.

--- server/post/bookings_post.js
+++ server/post/bookings_post.js
@@ -216,9 +216,27 @@ bookingRouter.post(
       const bookingDetails = await bookings_queries.getBookingDetails(
         booking_id
       );
+
+      const courtName = (
+        await courts_queries.retrieveCourt(bookingDetails.data.courtID)
+      ).data.court_name;
+
+      const courtTime = bookingDetails.data.time;
+
       const result = await bookings_queries.removeBooking(booking_id);
       if (result.result == true) {
         secondaryUsers = bookingDetails.data.secondaryUsers;
+
+        var dateFromMongoDB = new Date(bookingDetails.data.date);
+        var year = dateFromMongoDB.getFullYear();
+        var month = dateFromMongoDB.getMonth() + 1; // Months are zero-based, so add 1
+        var day = dateFromMongoDB.getDate();
+        var formattedDate =
+          year +
+          "-" +
+          (month < 10 ? "0" + month : month) +
+          "-" +
+          (day < 10 ? "0" + day : day);
 
         if (secondaryUsers.length > 0) {
           split_cost = bookingDetails.data.cost / (secondaryUsers.length + 1);
@@ -228,15 +246,23 @@ bookingRouter.post(
               sec_user.email,
               split_cost
             );
+            server_functions.sendCancellationSuccessMail(
+              email,
+              courtName,
+              formattedDate,
+              courtTime,
+              split_cost
+            );
           }
           return res.json({ result: true });
         } else {
           await user_queries.updateUserBalance(email, bookingDetails.data.cost);
           server_functions.sendCancellationSuccessMail(
             email,
-            bookingDetails.data.court_name,
-            bookingDetails.data.date,
-            bookingDetails.data.hour
+            courtName,
+            formattedDate,
+            courtTime,
+            court.data.price
           );
           return res.json({ result: true });
         }
