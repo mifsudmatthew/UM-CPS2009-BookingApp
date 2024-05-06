6342671bcf0a06a5c200922c4f2e99ca3c77cc7d
Matthew Mifsud <matthewmifsud2004@gmail.com>
2024-05-06 17:10:16 +0200

    Profile folder [Cln]: Commented

--- client/src/components/profile/Bookings.jsx
+++ client/src/components/profile/Bookings.jsx
@@ -17,7 +17,9 @@ import { useNotifications } from "../../context/NotificationContext";
 const Bookings = () => {
   const { user, updateToken } = useProfile(); // Retrieve user data once when the component mounts
   const [courts, setCourts] = useState([]); // State variable to store the list of courts
-  const [secondary, setSecCourts] = useState([]); // State variable to store the available Secondaeycourts
+  const [secondary, setSecCourts] = useState([]); // State variable to store the available Secondary courts
+
+  // Retrieve the storeNotification function from the context
   const { storeNotification } = useNotifications();
 
   /**
@@ -27,27 +29,33 @@ const Bookings = () => {
    * @returns {Promise<void>} A Promise that resolves when the booked courts are fetched.
    */
   useEffect(() => {
+    // Fetch the booked courts when the component loads
     const fetchBookedCourts = async () => {
-      const name = user.name;
-      const email = user.email;
-      const user_details = { name, email };
+      const name = user.name; // Get the user's name
+      const email = user.email; // Get the user's email
+      const user_details = { name, email }; // Create an object with the user's details
       try {
+        // Send a POST request to the server to get the booked courts
         const response = await Post("/api/getFutureBookings", user_details);
         console.log(response);
+        // Set the booked courts in the state variable
         setCourts(response);
 
+        // Send a POST request to the server to get the booked courts
         const response2 = await Post(
           "/api/getFutureSecondaryBookings",
           user_details
         );
         console.log(response2);
+
+        // Set the booked courts in the state variable
         setSecCourts(response2);
       } catch (error) {
         // Log an error if the request fails
         console.error("Error fetching booked courts: ", error);
       }
     };
-    fetchBookedCourts();
+    fetchBookedCourts(); // Call the fetchBookedCourts function
   }, [user]);
 
   /**
@@ -57,14 +65,18 @@ const Bookings = () => {
    */
   const cancelBooking = async (id, price) => {
     try {
+      // Send a POST request to the server to cancel the booking
       const response = await Post("/api/cancelBooking", {
         booking_id: id,
         price: price,
       });
+
+      // Display a success message if the booking is successfully cancelled
       if (response.result == true) {
-        storeNotification("Court Successfully Cancelled!");
-        toast.success("Court Successfully Cancelled!");
-        updateToken(await getUpdatedToken());
+        storeNotification("Court Successfully Cancelled!"); // Store a notification in local storage
+        toast.success("Court Successfully Cancelled!"); // Display a success toast
+        updateToken(await getUpdatedToken()); // Update the access token
+        // Remove the cancelled booking from the list of courts
         setCourts((prevCourts) =>
           prevCourts.filter((court) => court.id !== id)
         );
@@ -99,7 +111,8 @@ const Bookings = () => {
               {/* Map through upcomingBookings array and render each booking */}
               {courts.map((court) => {
                 const courtDateTime = new Date(
-                  new Date(court.date).setHours( // Set the date and time of the booking
+                  new Date(court.date).setHours(
+                    // Set the date and time of the booking
                     Number(court.time),
                     0 // Since the minutes are always 0
                   )
